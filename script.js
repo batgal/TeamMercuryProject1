73cb1a4a150775d6e5e0a767220c180cbfcabcf5
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-06-08 20:39:53 -0700

    added input and start functions in js

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-fetch("https://api.open5e.com/monsters/", {
+const fetchURL = "https://api.open5e.com/monsters/"
+fetch(fetchURL, {
   cache: "reload",
 })
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    console.log(data);
+  .then(function (monsters) {
+    console.log(monsters);
   });
+
+// function search (event) {
+//   var query = document.querySelector("btn");
+//   query.addEventListener("click", function() {;
+//     search();
+//   })
+//   fetch("https://api.open5e.com/monsters/" + query, {
+//     cache: "reload",
+//   })
+
+//   .then(function (response) {
+//     return response.json();
+//   })
+
+//   .then(function (monsters) {
+//     if (monsters.results[0]) {
+//       serveMonster(monsters.results[0]);
+//     } else {
+//       //make a statement for an error here
+//     }
+//   })}
+
+  function serveMonster(fetchURL) {
+    fetch(fetchURL, {
+      cache: "reload",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (monsters) {
+        console.log(monsters);
+      })
+      .then
+    console.log(results.then)
+    // var image = document.getElementById("monsterImage");
+    // if (monsters.img_main) {
+    //   image.src = monsters.img_main;
+    // }
+  }
+
+  serveMonster(fetchURL)
+
+// fetch("https://api.open5e.com/monsters/?search=fir", {
+  
