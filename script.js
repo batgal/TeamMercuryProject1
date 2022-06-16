20a5ea715f0352a0982bf48b7c33f69d42e1af28
Katlyn Green <katlyngreen@Katlyns-MacBook-Pro.local>
2022-06-15 21:09:09 -0400

    changed 2nd API call to fetch results - something in the last pull broke the code

--- script.js
+++ script.js
@@ -18,10 +18,9 @@ var formSubmitHandler = function (event) {
   } else {
     modal.classList.add("is-active");
   }
-  // Local Storage//
+  //Local Storage//
   var pastMonster = {
     monsterName: monsterName,
-    armorClass: 14,
   };
 
   localStorage.setItem("pastMonster", JSON.stringify(pastMonster));
@@ -123,15 +122,15 @@ var displayStats = function (monStat, monName) {
       monsterContainerEl.appendChild(repoEl);
     }
     if (monStat.results[i].name.toLowerCase() === monName.toLowerCase()) {
-      var immunities = monStat.results[i].condition_immunities;
+      var alignment = monStat.results[i].alignment;
 
       var repoEl = document.createElement("div");
       repoEl.classList =
         "list-item flex-row justify-space-between align-center";
-      repoEl.textContent = "Condition Immunities: ";
+      repoEl.textContent = "Alignment: ";
 
       var titleEl = document.createElement("span");
-      titleEl.textContent = immunities;
+      titleEl.textContent = alignment;
 
       repoEl.appendChild(titleEl);
 
@@ -146,61 +145,45 @@ var displayStats = function (monStat, monName) {
 };
 // second API dynamic HTML
 var displayXP = function (monStat, monName) {
-  if (monStat.length === 0) {
-    var displayXP = function (monStat, monName) {
-      if (!monStat) {
-        monsterContainerEl.textContent = "No monsters found.";
-        return;
-      }
-
-      searchMonster.textContent = monName;
-      console.log(monStat);
-      //tried using the same format for the other API but took out the name parameters since it's not in this path.
-      //also tried renaming everything so that theyre different
-      //this doesnt work but when i checked the console the div for this isnt showing up at all but idk if data needs to populate for the div to populategi
-      for (var i = 0; i < monStat.length; i++) {
-        console.log(monName);
-
-        if (monStat.toLowerCase() === monName.toLowerCase()) {
-          var experience = monStat.xp;
-
-          var xpEl = document.createElement("div");
-          xpEl.classList =
-            "list-item flex-row justify-space-between align-center";
-          xpEl.textContent = "XP gained: ";
-
-          var experience = monStat.xp;
-
-          var titleEl = document.createElement("span");
-          titleEl.textContent = experience;
-
-          xpEl.appendChild(titleEl);
-
-          var experienceEl = document.createElement("span");
-          experienceEl.classList = "flex-row align-center";
-
-          xpEl.appendChild(experienceEl);
-
-          monsterContainerEl.appendChild(xpEl);
-        }
-      }
-    };
-
-    userFormEl.addEventListener("submit", formSubmitHandler);
-
-    //Modal//
-    const huntButton = document.querySelector(".btn");
-    const modalBg = document.querySelector(".modal-background");
-    const modal = document.querySelector(".modal");
-
-    //Allows modal to be removed once background is clicked//
-    modalBg.addEventListener("click", () => {
-      modal.classList.remove("is-active");
-    });
-
-    //Allows modal to be removed once clicked//
-    modal.addEventListener("click", () => {
-      modal.classList.remove("is-active");
-    });
+  if (!monStat) {
+    monsterContainerEl.textContent = "No monsters found.";
+    return;
   }
+
+  searchMonster.textContent = monName;
+
+  var experience = monStat.xp;
+
+  var xpEl = document.createElement("div");
+  xpEl.classList = "list-item flex-row justify-space-between align-center";
+  xpEl.textContent = "XP gained: ";
+
+  var titleEl = document.createElement("span");
+  titleEl.textContent = experience;
+
+  xpEl.appendChild(titleEl);
+
+  var experienceEl = document.createElement("span");
+  experienceEl.classList = "flex-row align-center";
+
+  xpEl.appendChild(experienceEl);
+
+  monsterContainerEl.appendChild(xpEl);
 };
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+//Modal//
+const huntButton = document.querySelector(".btn");
+const modalBg = document.querySelector(".modal-background");
+const modal = document.querySelector(".modal");
+
+//Allows modal to be removed once background is clicked//
+modalBg.addEventListener("click", () => {
+  modal.classList.remove("is-active");
+});
+
+//Allows modal to be removed once clicked//
+modal.addEventListener("click", () => {
+  modal.classList.remove("is-active");
+});
